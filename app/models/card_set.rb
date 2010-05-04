2c7e8b92d7298ac35ebd0a70ec51e93afc0c874c
irkenInvader <errinlarsen@gmail.com>
2010-05-04 14:29:11 -0500

    Find a bug in rails 2.3.5 that creates duplicate entries in the Join Model when using 'accepts_nested_attributes_for'.
    Comment out 'validates_uniqueness_of' method call in the Membership model
    to work-around the bug.  Duplicate entries will continue until Rails 2.3.6.

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -2,6 +2,10 @@ class CardSet < ActiveRecord::Base
   has_many :memberships
   has_many :cards, :through => :memberships
 
+  # The following code is BROKEN in rails 2.3.5.  The accepts_nested_attributes_for
+  # method call creates duplicate entries in Membership for every nested model
+  # in the input (in this case, Cards) .For the time being, we'll have to accept duplicate
+  # entries in the Memberships model until 2.3.6 releases.
   accepts_nested_attributes_for :memberships, :cards
 
   validates_uniqueness_of :name
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,15 @@ class Membership < ActiveRecord::Base
   belongs_to :card
   belongs_to :card_set
 
-  validates_uniqueness_of :card_id, :scope => :card_set_id,
-                      :message => "may only be a member of the set once"
+  # The following code is BROKEN in rails 2.3.5.  For the time being, we'll
+  # have to accept duplicate entries in the Memberships model until 2.3.6 releases.
+  #
+  #   correction:
+  #   THIS code is not broken.  The broken code is in
+  #   CardSet.accepts_nested_attributes_for, which creates duplicate entries in
+  #   Membership for every nested model in the input (in this case, Cards) .
+  #
+
+#  validates_uniqueness_of :card_id, :scope => :card_set_id,
+#                      :message => "may only be a member of the set once"
 end
