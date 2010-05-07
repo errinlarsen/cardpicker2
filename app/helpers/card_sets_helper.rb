801bcfa466582b931a0abc2f71c58159045a7b89
irkenInvader <errinlarsen@gmail.com>
2010-05-06 19:04:33 -0500

    Override the 4 _path methods to handle multiple incoming URIs for both Cards views and CardSets views.
    Before this change, card_path translates to http://host/card/:id.  There
    is also a game_card_path due to the new routes, which translate to
    http://host/<game-name>/card/:id.  To alleviate clutter and try to be
    more DRY, I override the <model>_path methods in the respective helper
    modules to allow the original _path methods to handle both, calling
    game_card_path if a :game parameter is detected, and the original 'super'
    method otherwise.

--- app/helpers/card_sets_helper.rb
+++ app/helpers/card_sets_helper.rb
@@ -1,2 +1,33 @@
 module CardSetsHelper
+  def card_set_path( options = [] )
+    if @game.nil? || @game.empty?
+      super( options )
+    else
+      game_card_set_path( @game, options )
+    end
+  end
+
+  def card_sets_path
+    if @game.nil? || @game.empty?
+      super
+    else
+      game_card_sets_path
+    end
+  end
+
+  def edit_card_set_path( options = [] )
+    if @game.nil? || @game.empty?
+      super( options )
+    else
+      edit_game_card_set_path( @game, options )
+    end
+  end
+
+  def new_card_set_path
+    if @game.nil? || @game.empty?
+      super
+    else
+      new_game_card_set_path
+    end
+  end
 end
--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,33 @@
 module CardsHelper
+  def card_path( options = [] )
+    if @game.nil? || @game.empty?
+      super( options )
+    else
+      game_card_path( @game, options )
+    end
+  end
+
+  def cards_path
+    if @game.nil? || @game.empty?
+      super
+    else
+      game_cards_path
+    end
+  end
+
+  def edit_card_path( options = [] )
+    if @game.nil? || @game.empty?
+      super( options )
+    else
+      edit_game_card_path( @game, options )
+    end
+  end
+
+  def new_card_path
+    if @game.nil? || @game.empty?
+      super
+    else
+      new_game_card_path
+    end
+  end
 end
