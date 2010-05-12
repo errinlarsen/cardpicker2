6e350826d3112cbdfbeb6fd20854e62a7b6bc2bc
irkenInvader <errinlarsen@gmail.com>
2010-05-12 09:28:53 -0500

    Modify the new 'sort_cost' method in the :random action to better display the Alchemy cards in the view.

--- app/controllers/card_sets_controller.rb
+++ app/controllers/card_sets_controller.rb
@@ -32,7 +32,7 @@ class CardSetsController < ApplicationController
     @game = params[:game] ||= ""
     @card_set = CardSet.new( :name => "New Set of 10", :set_type => "Set of 10" )
     dominion_set = DominionSet.new( :bsw_style => true )
-    @cards = dominion_set.generate.sort { |a,b| a.cost <=> b.cost }
+    @cards = dominion_set.generate.sort { |a,b| a.sort_cost <=> b.sort_cost }
 
     respond_to do |format|
       format.html
