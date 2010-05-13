af932ed583b4b0e05d76d75c0ade9ce0fca90ddf
irkenInvader <errinlarsen@gmail.com>
2010-05-13 17:52:00 -0500

    create a list_for helper method to translate DominionSetOptions.to_hash output to an unordered list for rendering in an .erb view.

--- app/helpers/card_sets_helper.rb
+++ app/helpers/card_sets_helper.rb
@@ -30,4 +30,23 @@ module CardSetsHelper
       new_game_card_set_path
     end
   end
+
+  def list_for( options = {}, tag_options = nil, open = false, escape = true )
+    options.delete(:excludes) if options[:excludes].empty?
+    options.delete(:includes) if options[:includes].empty?
+    val = tag( "ul", tag_options, true, escape )
+    options.each do |key, value|
+      if value 
+        val << tag( "li", tag_options, true, escape )
+        val << "#{key.to_s.humanize}: "
+        if value.class == Array
+          val << "#{value.to_sentence}"
+        else
+          val << "#{value}"
+        end
+        val << "</li>"
+      end
+    end
+    val << "</ul>"
+  end
 end
