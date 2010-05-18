b0e87cb99b8c88d1fdd77cd2b08145ca2f7ad3ae
irkenInvader <errinlarsen@gmail.com>
2010-05-18 01:42:22 -0500

    Add dominion-specific named_scopes

--- app/models/card.rb
+++ app/models/card.rb
@@ -7,6 +7,23 @@ class Card < ActiveRecord::Base
   validates_presence_of :creator_id, :name, :game, :expansion, :card_type, :cost
 
   POTION_VALUE = 3
+
+  named_scope :dominion, lambda { |*args|
+    expansions = args.first || all.collect { |card| card.expansion }.uniq
+    { :conditions => { :game => 'dominion', :custom =>false, :expansion => expansions }}
+  } do
+    def all_expansions
+      all( :select => 'DISTINCT expansion').collect { |card| card.expansion }
+    end
+  end
+
+  named_scope :dominion_with_custom, lambda { |*expansions|
+    expansions = expansions.first || all.collect { |card| card.expansion }.uniq
+    { :conditions => { :game => 'dominion', :expansion => expansions }}
+  }
+
+  named_scope :start_player, :conditions => { :game => 'start_player', :custom => false }
+  named_scope :start_player_with_custom, :conditions => { :game => 'start_player' }
   
   def computed_cost
     chars = cost.chars
